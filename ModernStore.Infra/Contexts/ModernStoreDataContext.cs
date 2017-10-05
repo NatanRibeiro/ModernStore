5e1601030c2f6c7a1705fa9afad3adcfff753005
Natan Ribeiro Dutra <natan_r.dutra@hotmail.com>
2017-10-05 00:23:34 -0400

    Natan Ribeiro Dutra: Was Initialized the mapping for EF.

---
+++ ModernStore.Infra/Contexts/ModernStoreDataContext.cs
@@ -0,0 +1,21 @@
+﻿
+using ModernStore.Domain.Entities;
+using System.Data.Entity;
+
+namespace ModernStore.Infra.Contexts
+{
+    public class ModernStoreDataContext : DbContext
+    {
+        public ModernStoreDataContext()
+            //: base("ModernStoreConnectionString")
+            : base(@"Server=NATAN;Database=ModernStore;Trusted_Connection=True;")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        public DbSet<Customer> Customers {get; set;}
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ModernStore.Infra/Mappings/CustomerMap.cs
@@ -0,0 +1,22 @@
+﻿using ModernStore.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ModernStore.Infra.Mappings
+{
+    class CustomerMap : EntityTypeConfiguration<Customer>
+    {
+        public CustomerMap()
+        {
+            ToTable("Customer");
+            HasKey(x => x.Id);
+            Property(x => x.Birthdate);
+            Property(x => x.Document.Number);
+            Property(x => x.Email.Address);
+            Property(x => x.Name.FirstName);
+            Property(x => x.Name.LastName);
+            Property(x => x.User.Username);
+            Property(x => x.User.Password);
+            Property(x => x.User.Active);
+        }
+    }
+}
