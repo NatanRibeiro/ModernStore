13981acccf1676b757e7438e01f9c1f7e4b9ebef
Natan Ribeiro Dutra <natan_r.dutra@hotmail.com>
2017-11-13 22:28:06 -0500

    Natan Ribeiro Dutra:  Repositories and Transactions to work with Unit of Work (Commit/Rollback)

---
+++ ModernStore.Infra/Repositories/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using ModernStore.Domain.Entities;
+using ModernStore.Domain.Repositories;
+using ModernStore.Infra.Contexts;
+
+namespace ModernStore.Infra.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly ModernStoreDataContext _context;
+
+        public CustomerRepository(ModernStoreDataContext context) => _context = context;
+
+        public Customer Get(Guid id) => _context.Customers.Include(x => x.User).FirstOrDefault(x => x.Id == id);
+
+        public Customer Get(string document) => _context.Customers.Include(x => x.Document).FirstOrDefault(x => x.Document.Number == document);
+
+        public Customer GetByUserId(Guid id) => _context.Customers.Include(x => x.User).AsNoTracking().FirstOrDefault(x => x.User.Id == id);
+
+        public void Update(Customer customer) => _context.Entry(customer).State = EntityState.Modified;
+
+        public bool DocumentExists(string document) => _context.Customers.Include(x => x.Document).AsNoTracking().Any(x => x.Document.Number == document);
+
+        public void Save(Customer customer) => _context.Customers.Add(customer);
+    }
+}
---
+++ ModernStore.Infra/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using ModernStore.Domain.Entities;
+using ModernStore.Domain.Repositories;
+using ModernStore.Infra.Contexts;
+
+namespace ModernStore.Infra.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ModernStoreDataContext _context;
+
+        public ProductRepository(ModernStoreDataContext context) => _context = context;
+
+        public Product Get(Guid id) => _context.Products.AsNoTracking().FirstOrDefault(x => x.Id == id);
+
+        public void Save(Order order) => _context.Orders.Add(order);
+    }
+}
