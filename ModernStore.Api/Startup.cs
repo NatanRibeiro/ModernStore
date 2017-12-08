a0adfab6798c70d3250e104450cd275c267c5a8c
Natan Ribeiro Dutra <natan_r.dutra@hotmail.com>
2017-12-07 23:53:26 -0500

    Natan Ribeiro Dutra: Middleware started.

--- ModernStore.Api/Startup.cs
+++ ModernStore.Api/Startup.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using ModernStore.Api.Security;
 using ModernStore.Domain.Commands.Handlers;
 using ModernStore.Domain.Repositories;
 using ModernStore.Domain.Services;
@@ -8,15 +12,51 @@ using ModernStore.Infra.Contexts;
 using ModernStore.Infra.Repositories;
 using ModernStore.Infra.Services;
 using ModernStore.Infra.Transactions;
+using System.Text;
 
 namespace ModernStore.Api
 {
     public class Startup
     {
+        private const string ISSUER = "ISSUER";
+        private const string AUDIENCE = "AUDIENCE";
+        private const string SECRET_KEY = "B97F81B0-1D8C-44C5-87A1-D7DDC021104B";
+
+        private readonly SymmetricSecurityKey _sigingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SECRET_KEY));
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+            });
+
             services.AddCors();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("User", policy => policy.RequireClaim("ModernStore", "User"));
+                options.AddPolicy("Admin", policy => policy.RequireClaim("ModernStore", "Admin"));
+            });
+
+            //services.Configure<TokenOptions>(options =>
+            //{
+            //    options.Issuer = ISSUER;
+            //    options.Audience = AUDIENCE;
+            //    options.SigningCredentials = new SigningCredentials(_sigingKey, SecurityAlgorithms.HmacSha256);
+            //});
+
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //}).AddJwtBearer(options =>
+            //{
+            //    options.Audience = AUDIENCE;
+            //    options.Authority = Configuration["MySettings:Auth0Settings:Authority"];
+            //});
 
             services.AddScoped<ModernStoreDataContext, ModernStoreDataContext>();
             services.AddTransient<IUow, Uow>();
@@ -35,6 +75,23 @@ namespace ModernStore.Api
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = ISSUER,
+
+                ValidateAudience = true,
+                ValidAudience = AUDIENCE,
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _sigingKey,
+
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+
+                ClockSkew = System.TimeSpan.Zero
+            };
 
             app.UseCors(x =>
             {
